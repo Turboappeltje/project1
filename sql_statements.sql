ef0c885f31b6dcd8848932938e054a60e85b9246
jari <jg.frieswijk@hotmail.com>
2020-09-09 16:22:55 +0200

    Update huiswerk

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,10 +1,14 @@
+-- Aanmaken van database genaamd Project1
 CREATE DATABASE Project1
 
+-- Tabel genaamd account aanmaken met id email en password.
 CREATE TABLE Account ( 
 	ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	Email VARCHAR(255) UNIQUE,
     Password VARCHAR(255) 
 );
+
+-- Tabel genaamd Persoon met voornaam Tussenvoegsel achternaam en Gebruikersnaam.
 CREATE TABLE Persoon (
     Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     Voornaam VARCHAR(255),
