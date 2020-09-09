52d3b514f136e348aefc7c52e47e3917449eb0ba
jari <jg.frieswijk@hotmail.com>
2020-09-09 12:39:25 +0200

    Huiswerk sql

---
+++ sql_statements.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Project1
+
+CREATE TABLE Account ( 
+	ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	Email VARCHAR(255) UNIQUE,
+    Password VARCHAR(255) 
+);
+CREATE TABLE Persoon (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Voornaam VARCHAR(255),
+    Tussenvoegsel VARCHAR(255),
+    Achternaam VARCHAR(255),
+    Gebruikersnaam VARCHAR(255) UNIQUE,
+    Account_id INT,
+    FOREIGN KEY (Account_id) REFERENCES Account(id)
+); 
