4c68763b8eeff018f65d68c61158927b87dfd76f
jari <jg.frieswijk@hotmail.com>
2020-09-10 12:00:37 +0200

    Update

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,11 +1,13 @@
 -- Aanmaken van database genaamd Project1
 CREATE DATABASE Project1
+
 -- Tabel genaamd account aanmaken met id email en password.
 CREATE TABLE Account (
 ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 Email VARCHAR(255) UNIQUE,
 Password VARCHAR(255)
 );
+
 -- Tabel genaamd Persoon met voornaam Tussenvoegsel achternaam en Gebruikersnaam.
 CREATE TABLE Persoon (
 Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,4 +18,5 @@ Gebruikersnaam VARCHAR(255) UNIQUE,
 Account_id INT,
 FOREIGN KEY (Account_id) REFERENCES Account(id)
 );
+
 -- Primary key op de dezelfde regel werkt ook
